1db2e53b42a04b3d3e1bd042e61133ea621d60f8
Niranjan Ravichandran <niranjanrav@gmail.com>
2016-09-18 02:44:33 -0400

    Module renaming

--- modelObject.js
+++ modelObject.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  function PushMessage (alert, payload) {
+  PushMessage: function (alert, payload) {
     this.expiry = Math.floor(Date.now() / 1000) + 3600; // Default Expiry in 1 hour.
     this.badge = 3;
     this.sound = 'ping.aiff';
