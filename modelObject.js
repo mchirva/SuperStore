fb9adc93dc85adc56daf8f01e1b9a5ca28fb5865
Niranjan Ravichandran <niranjanrav@gmail.com>
2016-09-18 02:46:03 -0400

    Changed default alert

--- modelObject.js
+++ modelObject.js
@@ -4,7 +4,7 @@ module.exports = {
     this.expiry = Math.floor(Date.now() / 1000) + 3600; // Default Expiry in 1 hour.
     this.badge = 3;
     this.sound = 'ping.aiff';
-    this.alert = alert;
+    this.alert = "\uD83D\uDCE7".concat(alert);
     this.payload = payload;
   }
 
